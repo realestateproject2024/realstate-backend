eff5664c620d6b1e0ba6974c29ad30c5c6a248e6
shadab <gabuyama68@gmail.com>
2024-01-30 23:28:52 +0530

    wip

--- config/db.js
+++ config/db.js
@@ -1,39 +1,22 @@
-/*
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const pool = mysql.createPool({
- host: process.env.host,
- user: process.env.user,
- port: process.env.port,
- password: process.env.password,
- database: process.env.database,
+  host: "localhost",
+  user: "root",
+  port: 3001,
+  password: "Real@2024",
+  database: "realstate",
 });
-
-module.exports = pool.promise();
-*/
-
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 // const pool = mysql.createPool({
-//   host: 'db-dev.c9cx9dwnzthp.ap-northeast-1.rds.amazonaws.com',
-//   user: 'developer',
-//   port: 3306,
-//   password: 'developerars01',
-//   database: 'Ars',
+//   host: process.env.host,
+//   user: process.env.user,
+//   // port: process.env.port,
+//   password: process.env.password,
+//   database: process.env.database,
 // });
-
-const pool = mysql.createPool({
-  host: process.env.host,
-  user: process.env.user,
-  // port: process.env.port,
-  password: process.env.password,
-  database: process.env.database,
-});
 
 module.exports = pool.promise();
